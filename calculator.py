fc80fc9e2c6e0138270ee90de55636145548ece3
Anshika6305 <anshika.n.3065@gmail.com>
2024-09-09 22:16:51 +0530

    first

---
+++ calculator.py
@@ -0,0 +1,13 @@
+a=int(input("Enter any number: "))
+b=int(input("Enter another number: "))
+c=input("Enter the operaation sign: ")
+if(c=="+"):
+    print("Addition of given number is: ",a+b)
+elif(c=="-"):
+    print("subtraction of given number is: ",a-b)
+elif(c=="*"):
+    print("Multiplication of given number is: ",a*b)
+elif(d=="/"):
+    print("Division of given number is: ",a/b)
+else:
+    print("OOPS! Somthing Went Wrong.........")
